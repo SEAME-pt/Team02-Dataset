07263bbe1f9e6c2fe6b5de0e3c46f65b301c48b7
Luis Carvalho <filip.carvalho12@gmail.com>
2025-07-04 15:33:07 +0100

    cenas

--- SEAME/extract_frames.py
+++ SEAME/extract_frames.py
@@ -36,7 +36,7 @@ def extract_unique_frames(video_path, output_folder, similarity_threshold=0.80, 
     
     # Initialize variables
     frame_number = 0
-    saved_count = 6100
+    saved_count = 7000
     prev_frame_gray = None
     last_saved_frame_gray = None
     
@@ -85,7 +85,7 @@ def extract_unique_frames(video_path, output_folder, similarity_threshold=0.80, 
     print(f"Achieved {saved_count/frame_count*100:.2f}% compression ratio")
 
 if __name__ == "__main__":
-    video_path = "assets/trinta.mp4"
+    video_path = "assets/lights.mp4"
     output_folder = "framesTrafficLights"
     
     extract_unique_frames(
